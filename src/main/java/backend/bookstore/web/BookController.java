67531a2ae7178abef35c08500d2f46b0dfe10139
sanaz.jaber.ansari@myy.haaga-helia.fi <sanaz.jaber.ansari@myy.haaga-helia.fi>
2026-02-03 10:33:04 +0200

    korjattu delete ja save

--- src/main/java/backend/bookstore/web/BookController.java
+++ src/main/java/backend/bookstore/web/BookController.java
@@ -1,25 +1,23 @@
 package backend.bookstore.web;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import backend.bookstore.domain.Book;
 import backend.bookstore.domain.BookRepository;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
-
 
 
 @Controller
 public class BookController {
-
+    
+    private static final Logger log =
+        LoggerFactory.getLogger(BookController.class);
     private final BookRepository bookRepository;
 
     public BookController(BookRepository bookRepository) {
@@ -39,7 +37,7 @@ public class BookController {
     }
     
     @PostMapping("/saveBook")
-    public String addBook(@ModelAttribute Book book, Model model) {
+    public String addBook(Book book, Model model) {
             log.info("Kirja " + book.toString());
             bookRepository.save(book);
         return "redirect:/books";
@@ -48,7 +46,7 @@ public class BookController {
     @GetMapping("/deleteBook/{id}")
     public String deleteBook(@PathVariable("id") Long id) {
         bookRepository.deleteById(id);
-        return "redirect: /books";
+        return "redirect:/books";
     }
     
     
